d5a4d7cb379e15c1e251b89b2ac8d78f9fe3488b
JasterStary <jasterstary@gmail.com>
2026-02-02 22:51:36 +0100

    Updated 2026-02-02

--- pkg/loader.js
+++ pkg/loader.js
@@ -1,5 +1,10 @@
 
-// npm install --save ...
+import mixitup from 'mixitup';
+import mixitupPagination from 'mixitup-pagination';
+window.mixitup = mixitup;
+// Call the mixitup factory's .use() method, passing in the extension to install it
+window.mixitup.use(mixitupPagination);
+//import "mixitup-multifilter/dist/mixitup-multifilter.js";
 
 import "./plugin.js";
 import "./style.css";
